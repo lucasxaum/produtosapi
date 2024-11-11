4d187e4c3fcf3b41cd7df6a0f439ec3e1c74941a
Lucas J. <lucas_junio_051191@hotmail.com>
2024-11-11 09:37:19 -0300

    17 e 18. Atualizando dados e realizando busca com parâmetros.

--- src/main/java/io/github/lucasxaum/produtosapi/repository/ProdutoRepository.java
+++ src/main/java/io/github/lucasxaum/produtosapi/repository/ProdutoRepository.java
@@ -3,6 +3,8 @@ package io.github.lucasxaum.produtosapi.repository;
 import io.github.lucasxaum.produtosapi.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProdutoRepository extends JpaRepository<Produto, String> {
+import java.util.List;
 
+public interface ProdutoRepository extends JpaRepository<Produto, String> {
+    List<Produto> findByNome(String nome);
 }
